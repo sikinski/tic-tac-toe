f299dff0ce2fefe878adcbe252078184f68b5e8e
Si Kinski <ulyana.sikinski@gmail.com>
2022-02-18 08:34:34 +0500

    render bottom buttons

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -5,6 +5,8 @@ import Konva from 'konva';
 export class Game {
     stage: Stage;
     public numCell: number = 3
+    public btnArea: Array<object> = []
+    public numPlayers: number = 1
 
     constructor(idContainer: string, width: number, height: number) {
         this.stage = new Konva.Stage({
@@ -19,7 +21,7 @@ export class Game {
 
         const heading = new Konva.Text({
             x: this.stage.width() / 2,
-            y: 15,
+            y: 10,
             text: 'Tic-tac-toe',
             fontSize: 36,
             fontFamily: 'Averta-Bold',
@@ -32,9 +34,9 @@ export class Game {
     renderField() {
         const layer = new Konva.Layer();
 
-        const dimensionField: number = 400
+        const dimensionField: number = 380
         const offsetX: number = this.stage.width() / 2 - dimensionField / 2
-        const offsetY: number = this.stage.height() / 2 - dimensionField / 2
+        const offsetY: number = (this.stage.height() / 2 - dimensionField / 2) - 20
 
         const field = new Konva.Rect({
             x: offsetX,
@@ -71,9 +73,60 @@ export class Game {
         layer.add(grid)
         this.stage.add(layer);
     }
+    renderButtons() {
+        const layer = new Konva.Layer()
+
+        const widthBtn = 160
+        const heightBtn = 50
+        const margin = 25
+
+        const names = ['changeXO', 'restart', `players (${this.numPlayers})`]
+
+        const groupBtns = new Konva.Group({
+            x: this.stage.width() / 2 - (widthBtn * names.length + margin) / 2,
+            y: this.stage.height() - heightBtn * 2,
+        });
+
+        for (let i = 0; i < names.length; i++) {
+            const btn = new Konva.Rect({
+                x: i * widthBtn + i * margin,
+                y: 13,
+                width: widthBtn,
+                height: heightBtn,
+                name: names[i],
+                fill: '#1f1f1f',
+                stroke: '#1f1f1f',
+                strokeWidth: 2,
+                shadowBlur: 15,
+                cornerRadius: 14,
+            });
+
+            const text = new Konva.Text({
+                x: btn.x(),
+                y: btn.y(),
+                text: `${names[i]}`,
+                fontSize: 16,
+                fontFamily: 'Averta-Bold',
+                fill: 'white',
+                width: btn.width(),
+                padding: 20,
+                align: 'center',
+                verticalAlign: 'middle',
+            })
+
+            this.btnArea.push({ name: btn.name(), x: btn.x(), y: btn.y(), width: widthBtn, height: heightBtn }) // coords inside the group!
+
+            groupBtns.add(btn)
+            groupBtns.add(text)
+        }
+
+        layer.add(groupBtns)
+        this.stage.add(layer)
+    }
 
     initRender() {
         this.renderHeading()
         this.renderField()
+        this.renderButtons()
     }
 }
